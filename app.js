392acc5d8f2d57a0545d939af01f837366466364
Muhammmad Sufiyan <msufiyan24437@gmail.com>
2024-09-25 19:44:24 +0500

    table

---
+++ app.js
@@ -0,0 +1,9 @@
+var table = +prompt("Enter table number:");
+
+for(var i =1; i<=table; table--){
+
+for(var h = 1; h<=10; h++){
+document.write(table + " x " + h + " = " , h*table,  "<br>")
+}
+document.write("<br>") 
+}
